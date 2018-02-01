b3b3d83848c592e286d08a1f019d876da6ca4d1a
Tristan Peng <pengt@catlin.edu>
2018-01-31 17:22:23 -0800

    Finished analysis app

--- analysis/app.js
+++ analysis/app.js
@@ -53,9 +53,12 @@ for (var i = 0; i < data.length; i++) {
 fs.writeFileSync('export/tags.json', JSON.stringify(totTags));
 fs.writeFileSync('resources/team-cube-data.json', JSON.stringify(teamCubeData));
 var rankings = analyzeRank();
+if (!fs.existsSync('pit-data')) {
+  fs.mkdirSync('pit-data');
+}
 
 // Functions
-String.prototype.upperFirstLetter = function () {
+String.prototype.titleCase = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
 function analyzeTags(a) {
@@ -346,7 +349,7 @@ function matchDisplay(a) {
               <td>` + data[i].match + `</td>
               <td>` + scouts[data[i].scout] + `</td>
               <td>` + (data[i]['role'].indexOf('r') >= 0 ? data[i].redPlatformCube : data[i].bluePlatformCube) + `</td>
-              <td>` + (data[i]['role'].indexOf('r') >= 0 ? data[i]['redPlatform'].upperFirstLetter() : data[i]['bluePlatform'].upperFirstLetter()) + `</td>
+              <td>` + (data[i]['role'].indexOf('r') >= 0 ? data[i]['redPlatform'].titleCase() : data[i]['bluePlatform'].titleCase()) + `</td>
               <td>` + data[i].climbNotes + `</td>
             </tr>
           `);
@@ -399,6 +402,152 @@ function matchDisplay(a) {
           break;
       }
     }
+  };
+};
+function pit() {
+  var pit = [];
+  var pitfest = JSON.parse(fs.readFileSync('pit-data/manifest.json', 'utf8'));
+  for (var i = 0; i < pitfest.length; i++) {
+    var tempPit = JSON.parse(fs.readFileSync('pit-data/' + pitfest[i], 'utf8'));
+    if (tempPit.team != undefined) {
+      pit.push(tempPit);
+    }
+  };
+  $('.cell-pit-data-1')
+    .html(`
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th style="position: sticky; left: -1.5vw; width: 6em; background: white;">Team</th>
+            <th style="white-space: nowrap;">Scout</th>
+            <th style="white-space: nowrap;">Phone Number</th>
+            <th style="white-space: nowrap;">Left Auto</th>
+            <th style="white-space: nowrap;">Mid Auto</th>
+            <th style="white-space: nowrap;">Right Auto</th>
+            <th style="white-space: nowrap;">Preferred Role</th>
+            <th style="white-space: nowrap;">Notes</th>
+            <th style="white-space: nowrap;">Climb Type</th>
+            <th style="white-space: nowrap;">Notes</th>
+            <th style="white-space: nowrap;">Cube Load Method</th>
+            <th style="white-space: nowrap;">Cube Load Location</th>
+            <th style="white-space: nowrap;">Robot Code Language</th>
+            <th style="white-space: nowrap;">Drive Train</th>
+            <th style="white-space: nowrap;">Weight</th>
+          </tr>
+        </thead>
+        <tbody class="pit-table"></tbody>
+      </table>
+    `)
+    .css({'width': '90vw', 'overflow-x': 'scroll'})
+    .parent().after('<br><br>');
+  for (var i = 0; i < pit.length; i++) {
+    var pitKey = Object.keys(pit[i]);
+    var left = [];
+    var mid = [];
+    var right = [];
+    for (var j = 0; j < pitKey.length; j++) {
+      if (typeof pit[i][pitKey[j]] == 'object') {
+        if (pit[i][pitKey[j]].indexOf('Left Auto') >= 0) {
+          switch (pitKey[j]) {
+            case 'leftSwitchAuto':
+              left.push(' Left Switch');
+              break;
+            case 'rightSwitchAuto':
+              left.push(' Right Switch');
+              break;
+            case 'leftScaleAuto':
+              left.push(' Left Scale');
+              break;
+            case 'rightScaleAuto':
+              left.push(' Right Scale');
+              break;
+            case 'twoCubeAuto':
+              left.push(' Two Cube');
+              break;
+            case 'exchangeAuto':
+              left.push(' Exchange');
+              break;
+            case 'lineAuto':
+              left.push(' Cross Line');
+              break;
+          };
+        }
+        if (pit[i][pitKey[j]].indexOf('Mid Auto') >= 0) {
+          switch (pitKey[j]) {
+            case 'leftSwitchAuto':
+              mid.push(' Left Switch');
+              break;
+            case 'rightSwitchAuto':
+              mid.push(' Right Switch');
+              break;
+            case 'leftScaleAuto':
+              mid.push(' Left Scale');
+              break;
+            case 'rightScaleAuto':
+              mid.push(' Right Scale');
+              break;
+            case 'twoCubeAuto':
+              mid.push(' Two Cube');
+              break;
+            case 'exchangeAuto':
+              mid.push(' Exchange');
+              break;
+            case 'lineAuto':
+              mid.push(' Cross Line');
+              break;
+          };
+        }
+        if (pit[i][pitKey[j]].indexOf('Right Auto') >= 0) {
+          switch (pitKey[j]) {
+            case 'leftSwitchAuto':
+              right.push(' Left Switch');
+              break;
+            case 'rightSwitchAuto':
+              right.push(' Right Switch');
+              break;
+            case 'leftScaleAuto':
+              right.push(' Left Scale');
+              break;
+            case 'rightScaleAuto':
+              right.push(' Right Scale');
+              break;
+            case 'twoCubeAuto':
+              right.push(' Two Cube');
+              break;
+            case 'exchangeAuto':
+              right.push(' Exchange');
+              break;
+            case 'lineAuto':
+              right.push(' Cross Line');
+              break;
+          };
+        }
+      }
+    };
+    if (pit[i]['cubeLoad'] != undefined && typeof pit[i]['cubeLoad'] == 'object') {
+      for (var j = 0; j < pit[i]['cubeLoad'].length; j++) {
+        pit[i]['cubeLoad'][j] = ' ' + pit[i]['cubeLoad'];
+      };
+    }
+    $('.pit-table').append(`
+      <tr>
+        <td style="position: sticky; left: -1.5vw; width: 6em; background: white;">` + pit[i].team + `</td>
+        <td style="white-space: nowrap;">` + scouts[pit[i].scout] + `</td>
+        <td style="white-space: nowrap;">` + pit[i].phone + ' (' + (JSON.parse(pit[i].occupation) ? 'Mentor': 'Student') + `)</td>
+        <td style="white-space: nowrap;">` + left + `</td>
+        <td style="white-space: nowrap;">` + mid + `</td>
+        <td style="white-space: nowrap;">` + right + `</td>
+        <td style="white-space: nowrap;">` + pit[i].robotRole + `</td>
+        <td style="white-space: nowrap;">` + pit[i].robotRoleNotes + `</td>
+        <td style="white-space: nowrap;">` + (pit[i]['climb'] == 'false' ? 'No Climb :(' : pit[i]['climb'].titleCase()) + `</td>
+        <td style="white-space: nowrap;">` + pit[i].climbNotes + `</td>
+        <td style="white-space: nowrap;">` + pit[i].cubeLoadMethod + `</td>
+        <td style="white-space: nowrap;">` + pit[i].cubeLoad + `</td>
+        <td style="white-space: nowrap;">` + pit[i].lang + `</td>
+        <td style="white-space: nowrap;">` + pit[i].driveTrain + `</td>
+        <td style="white-space: nowrap;">` + pit[i].weight + `</td>
+      </tr>
+    `);
   };
 };
 
@@ -580,6 +729,7 @@ $('.input-team-lookup > input').keydown(function () {
 
 // Pit Data
 scout.page('Pit Data', [12]);
+pit();
 
 // Rankings
 scout.page('Rankings', [3, 3, 3, 3]);
